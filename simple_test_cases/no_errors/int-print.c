c625ebf0b45cdbd40aac6fe5d0262da78f34bd77
agnithamohan <30794745+agnithamohan@users.noreply.github.com>
2018-01-10 16:17:19 +0530

    Add files via upload

---
+++ simple_test_cases/no_errors/int-print.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a; 
+	a=10;  
+	printf("%d\n", a);
+	
+	return 0; 
+
+}
---
+++ simple_test_cases/no_errors/single-line-comment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+
+	int a=12;
+	int b=14;
+	int c;	
+	c=a+b;	//addition 
+	printf("C:%d",c); 
+			
+	
+	
+}
