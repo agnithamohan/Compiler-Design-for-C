6a4fb9f25d9ede9f5ab3f63090ac6a7a8814f25a
agnithamohan <30794745+agnithamohan@users.noreply.github.com>
2018-01-20 10:28:01 +0530

    Add files via upload

---
+++ Lexical_Analysis/Test-Cases/Complex-Test-Cases/With-Errors/tes-case-errors.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+	int a,b; //integer declaration 
+	
+	a=5;
+	b=8;	
+		
+	int c=a+b; 
+	
+	printf("Sum is %d , c ;
+	
+	/*Program to calculate 
+		sum of 2 integers 
+
+
+	
