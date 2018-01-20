24e9f05ec90abaa7b55b32d5792cff1e2da65f13
agnithamohan <30794745+agnithamohan@users.noreply.github.com>
2018-01-20 10:23:03 +0530

    Add files via upload

---
+++ Lexical_Analysis/Test-Cases/Simple-Test-Cases/No-Errors/int-float-print.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a; 
+	a=10; 
+	float b; 
+	b=10.6; 
+	printf("%d%f", a, b);
+	
+	return 0; 
+
+}
---
+++ Lexical_Analysis/Test-Cases/Simple-Test-Cases/No-Errors/multi-line-comment.c
@@ -0,0 +1,12 @@
+
+/* This is a simple program
+	with a multiline comment
+*/
+
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Hello!");
+} 	
