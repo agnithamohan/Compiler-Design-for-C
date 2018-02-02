6c1c204cbe6a2f39849727236b342d1fc15c7f3a
agnithamohan <30794745+agnithamohan@users.noreply.github.com>
2018-02-03 00:18:52 +0530

    Add files via upload

---
+++ Syntax_Analysis/Without_Errors/basic-if-else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int a=5;
+	if(a<10)
+	  {
+		a=a+1;
+		printf("Value of a is %d",a); 
+	   }
+	else
+	  {
+		a=a-1;		
+		printf("Value of a is %d",a); 
+	   }
+	
+}
